d8932bfe31679f75c5de002f1908d36465c55a77
hootalex <alexdangersky@gmail.com>
2021-12-21 13:45:07 +0100

    testing with the end of the

--- records.js
+++ records.js
@@ -1,25 +1,4 @@
 const records =  [
-   {
-    "name": "Coffee Shop on a Snowy Day",
-    "desc": "This diner was really just called \“Coffee Shop\”. Not many people were inside because of the snow. It has unfortunately since closed down in 2021.",
-    "geohash" : "dr5rswc025sw",
-    "time": "2018.03.22",
-    "url" : "https://hootalex.github.io/nyc-field-recordings/recordings/2018.03.22%20-%20Coffee%20Shop%20on%20a%20Snowy%20Day%20NYC%20-%20dr5rswc025sw.m4a"
-  },
-  {
-   "name": "NY Art Central Closing Day",
-   "desc": "The historic 111-year-old independent art supply store’s last day. Too sad for words.",
-   "geohash" : "dr5rsqgh6kdn",
-   "time": "2016.09.02",
-   "url" : "https://hootalex.github.io/nyc-field-recordings/recordings/2016.09.02%20-%20NY%20Art%20Central%20Closing%20Day-%20dr5rsqgh6kdn.m4a"
- },
- {
-  "name": "Grand Central on a Busy Afternoon",
-  "desc": "Hanging out by the clock in a very packed Grand Central Terminal.",
-  "geohash" : "dr5rudg4xj9d",
-  "time": "2018.04.02",
-  "url" : "https://hootalex.github.io/nyc-field-recordings/recordings/2018.04.02%20-%20Grand%20Central%20on%20a%20Busy%20Afternoon%20-%20dr5rudg4xj9d.m4a"
-},
 {
  "name": "Not real data just to test this",
  "desc": "Hanging out by the clock in a very packed Grand Central Terminal.",
@@ -13788,6 +13767,27 @@ const records =  [
  "geohash" : "abcdefghijklmno",
  "time": "2018.04.01",
  "url" : "https://hootalex.github.io/nyc-field-recordings/recordings/2018.04.02%20-%20Grand%20Central%20on%20a%20Busy%20Afternoon%20-%20dr5rudg4xj9d.m4a"
+},
+{
+ "name": "Coffee Shop on a Snowy Day",
+ "desc": "This diner was really just called \“Coffee Shop\”. Not many people were inside because of the snow. It has unfortunately since closed down in 2021.",
+ "geohash" : "dr5rswc025sw",
+ "time": "2018.03.22",
+ "url" : "https://hootalex.github.io/nyc-field-recordings/recordings/2018.03.22%20-%20Coffee%20Shop%20on%20a%20Snowy%20Day%20NYC%20-%20dr5rswc025sw.m4a"
+},
+{
+"name": "NY Art Central Closing Day",
+"desc": "The historic 111-year-old independent art supply store’s last day. Too sad for words.",
+"geohash" : "dr5rsqgh6kdn",
+"time": "2016.09.02",
+"url" : "https://hootalex.github.io/nyc-field-recordings/recordings/2016.09.02%20-%20NY%20Art%20Central%20Closing%20Day-%20dr5rsqgh6kdn.m4a"
+},
+{
+"name": "Grand Central on a Busy Afternoon",
+"desc": "Hanging out by the clock in a very packed Grand Central Terminal.",
+"geohash" : "dr5rudg4xj9d",
+"time": "2018.04.02",
+"url" : "https://hootalex.github.io/nyc-field-recordings/recordings/2018.04.02%20-%20Grand%20Central%20on%20a%20Busy%20Afternoon%20-%20dr5rudg4xj9d.m4a"
 }
 
 ]
